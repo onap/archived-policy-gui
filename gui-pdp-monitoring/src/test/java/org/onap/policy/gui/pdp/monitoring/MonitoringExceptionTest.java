d727556bb249a8e0b41a6d2f025ab6c9eaddabeb
Pamela Dragosh <pdragosh@research.att.com>
2020-06-23 10:14:58 -0400

    Changes for checkstyle 8.32
    Issue-ID: POLICY-2188
    Change-Id: I87701431e5f5c248ed7240eb5cdb7ca0682434c1
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- gui-pdp-monitoring/src/test/java/org/onap/policy/gui/pdp/monitoring/MonitoringExceptionTest.java
+++ gui-pdp-monitoring/src/test/java/org/onap/policy/gui/pdp/monitoring/MonitoringExceptionTest.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2020 Nordix Foundation.
+ *  Modifications Copyright (C) 2020 AT&T Inc.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +24,6 @@ package org.onap.policy.gui.pdp.monitoring;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
 import org.onap.policy.common.utils.test.ExceptionsTester;
 
 /**
