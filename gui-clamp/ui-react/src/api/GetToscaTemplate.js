451ebd54ef032ea06167045dc20372ae97660823
saul.gill <saul.gill@est.tech>
2021-06-03 18:06:18 +0100

    Added GET Tosca Service Template components
    Added in header, using existing styling
    Integrated with existing FE
    
    Issue-ID: POLICY-3222
    Change-Id: I5b6a9f4be3296b2969baa559a7a48c87d2ab9258
    Signed-off-by: saul.gill <saul.gill@est.tech>

---
+++ gui-clamp/ui-react/src/api/GetToscaTemplate.js
@@ -0,0 +1,56 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 20XX-20YY Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+
+const GetToscaTemplate = (props) => {
+
+  const [windowLocationPathName, setWindowLocationPathname] = useState('');
+
+  const getTemplateHandler = async () => {
+
+    setWindowLocationPathname(window.location.pathname);
+
+    const params = {
+      name: props.templateName,
+      version: props.templateVersion
+    }
+
+    const response = await fetch(windowLocationPathName +
+      '/restservices/clds/v2/toscaControlLoop/getToscaTemplate' + '?' + (new URLSearchParams(params)));
+
+    const data = await response.json();
+
+    props.onGetToscaServiceTemplate(data);
+
+  }
+
+  return (
+    <React.Fragment>
+      <Button variant="primary"
+              type="submit"
+              onClick={ getTemplateHandler }>Get Tosca Service Template</Button>
+    </React.Fragment>
+  );
+
+
+}
+
+export default GetToscaTemplate;
