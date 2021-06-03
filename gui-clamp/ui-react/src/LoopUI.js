80882869b0f8632748397190abbf6147e86abf42
saul.gill <saul.gill@est.tech>
2021-06-03 18:06:18 +0100

    Added GET Tosca Service Template components
    Added in header, using existing styling
    Integrated with existing FE
    
    Issue-ID: POLICY-3222
    Change-Id: I5b6a9f4be3296b2969baa559a7a48c87d2ab9258
    Signed-off-by: saul.gill <saul.gill@est.tech>

--- gui-clamp/ui-react/src/LoopUI.js
+++ gui-clamp/ui-react/src/LoopUI.js
@@ -1,26 +1,22 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP CLAMP
- * ================================================================================
- * Copyright (C) 2019, 2021 AT&T Intellectual Property. All rights
- *                             reserved.
+ *  Copyright (C) 2021 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END============================================
- * ===================================================================
  *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
  */
-
 import React from 'react';
 import styled from 'styled-components';
 import MenuBar from './components/menu/MenuBar';
@@ -54,6 +50,7 @@ import Alert from 'react-bootstrap/Alert';
 import Spinner from 'react-bootstrap/Spinner';
 
 import { Link } from 'react-router-dom';
+import ReadAndConvertYaml from "./components/dialogs/ReadAndConvertYaml";
 
 const StyledMainDiv = styled.div`
   background-color: ${ props => props.theme.backgroundColor };
@@ -298,6 +295,7 @@ export default class LoopUI extends React.Component {
   renderRoutes() {
     return (
       <React.Fragment>
+        <Route path="/readToscaTemplate" render={ (routeProps) => (<ReadAndConvertYaml { ...routeProps } />) }/>
         <Route path="/viewLoopTemplatesModal" render={ (routeProps) => (<ViewLoopTemplatesModal { ...routeProps } />) }/>
         <Route path="/manageDictionaries" render={ (routeProps) => (<ManageDictionaries { ...routeProps } />) }/>
         <Route path="/viewAllPolicies" render={ (routeProps) => (<ViewAllPolicies { ...routeProps } />) }/>
