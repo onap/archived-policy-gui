c22316601c225204361d193a7aa551f2e4bf7f58
saul.gill <saul.gill@est.tech>
2021-06-03 18:06:18 +0100

    Added GET Tosca Service Template components
    Added in header, using existing styling
    Integrated with existing FE
    
    Issue-ID: POLICY-3222
    Change-Id: I5b6a9f4be3296b2969baa559a7a48c87d2ab9258
    Signed-off-by: saul.gill <saul.gill@est.tech>

---
+++ gui-clamp/ui-react/src/components/dialogs/ReadAndConvertYaml.js
@@ -0,0 +1,89 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+import React, { useState } from "react";
+import GetToscaTemplate from "../../api/GetToscaTemplate";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+import styled from 'styled-components';
+
+const ModalStyled = styled(Modal)`
+  background-color: transparent;
+`
+
+const ErrMsgStyled = styled.div`
+  color: red;
+`
+
+const PreStyled = styled.pre`
+  color: #7F0055;
+  overflow: auto;
+  max-height: 70vh;
+`
+
+const ReadAndConvertYaml = (props) => {
+  const [show, setShow] = useState(true);
+  const [toscaTemplateData, setToscaTemplateData] = useState();
+  const name = 'ToscaServiceTemplateSimple';
+  const version = '1.0.0';
+
+  const handleClose = () => {
+    setShow(false);
+    props.history.push('/');
+  }
+
+  const getToscaServiceTemplateHandler = (toscaServiceTemplate) => {
+    const toscaData = {
+      ...toscaServiceTemplate,
+      id: Math.random().toString()
+    };
+    console.log(toscaData);
+    setToscaTemplateData(toscaData);
+  }
+
+  return (
+    <ModalStyled size="xl"
+                 show={ show }
+                 onHide={ handleClose }
+                 backdrop="static"
+                 keyboard={ false }>
+      <Modal.Header closeButton>
+        <Modal.Title>View Tosca Template</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <GetToscaTemplate templateName={ name }
+                          templateVersion={ version }
+                          onGetToscaServiceTemplate={ getToscaServiceTemplateHandler }/>
+        <PreStyled>{ JSON.stringify(toscaTemplateData, null, 2) }</PreStyled>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary"
+                type="null"
+                onClick={ handleClose }>Cancel</Button>
+      </Modal.Footer>
+    </ModalStyled>
+  );
+}
+
+export default ReadAndConvertYaml;
