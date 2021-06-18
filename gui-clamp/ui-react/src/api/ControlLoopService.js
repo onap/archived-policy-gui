552460263aab6e11b20ac564ceb0a7d0e0b1605d
brunomilitzer <bruno.militzer@est.tech>
2021-06-18 16:07:16 +0100

    Add Tosca Monitoring App to Policy Gui
    Implemented the prototype monitoring gui onto the policy gui
    
    Issue-ID: POLICY-3357
    Change-Id: Iabf25cd6c26d4e65a7a963cdd79c25aa91c39736
    Signed-off-by: brunomilitzer <bruno.militzer@est.tech>

---
+++ gui-clamp/ui-react/src/api/ControlLoopService.js
@@ -0,0 +1,47 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+export default class ControlLoopService {
+
+  static async getControlLoopList(windowLocationPathname) {
+
+    return await fetch(windowLocationPathname + '/restservices/clds/v2/toscaControlLoop/getToscaInstantiation', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin',
+    }).then(response => {
+      console.log("getControlLoopList received " + response.status);
+
+      if (response.ok) {
+        console.info("getControlLoopList query successful");
+        return response.json();
+      } else {
+        return response.text().then(responseBody => {
+          throw Error("HTTP " + response.status + "," + responseBody);
+        });
+      }
+    }).catch(error => {
+      console.error("getControlLoopList error occurred ", error);
+      alert("getControlLoopList error occurred " + error);
+      return undefined;
+    });
+  }
+}
---
+++ gui-clamp/ui-react/src/components/dialogs/ControlLoop/ControlLoopItem.js
@@ -0,0 +1,128 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+import React from "react";
+import styled from 'styled-components';
+
+import { Accordion, Button, Card } from "react-bootstrap";
+
+const AccordionBody = styled.div`
+  margin: 0;
+  padding: 0;
+  border: 1px solid #7f7f7f;
+  border-radius: 0;
+`
+
+const CardBody = styled(Card.Body)`
+  padding: 0;
+  margin: 0;
+`
+
+const UninitialisedHeader = styled.div`
+  margin: 0;
+  padding: 0 0 1px 0;
+  border-bottom: 1px solid #7f7f7f;
+  background: #cccccc;
+  font-weight: normal;
+  border-radius: 0;
+`
+
+const PassiveHeader = styled.div`
+  margin: 0;
+  padding: 0 0 1px 0;
+  border-bottom: 1px solid #7f7f7f;
+  background: #ffe87c;
+  font-weight: normal;
+  border-radius: 0;
+`
+
+const RunningHeader = styled.div`
+  margin: 0;
+  padding: 0 0 1px 0;
+  border-bottom: 1px solid #7f7f7f;
+  background: #7ec699;
+  font-weight: normal;
+  border-radius: 0;
+`
+
+const ToggleButton = styled(Button)`
+  color: #000000;
+  text-decoration: none;
+  
+  :hover, :active {
+    color: #000000;
+    text-decoration: none !important;
+  }  
+`
+
+const ControlLoopItem = (props) => {
+  const toggleState = () => {
+    switch (props.orderedState) {
+      case 'UNINITIALISED':
+        return renderUninitialisedOrderedState()
+      case 'PASSIVE':
+        return renderPassiveOrderedState();
+      case 'RUNNING':
+        return renderRunningOrderedState();
+    }
+  }
+
+  const renderUninitialisedOrderedState = () => {
+    return (
+      <UninitialisedHeader className="panel-header">
+        <Accordion.Toggle as={ToggleButton} variant="link" eventKey={ props.index.toString() }>
+          { props.title }
+        </Accordion.Toggle>
+      </UninitialisedHeader>
+    )
+  }
+
+  const renderPassiveOrderedState = () => {
+    return (
+      <PassiveHeader className="panel-header">
+        <Accordion.Toggle as={ToggleButton} variant="link" eventKey={ props.index.toString() }>
+          { props.title }
+        </Accordion.Toggle>
+      </PassiveHeader>
+    )
+  }
+
+  const renderRunningOrderedState = () => {
+    return (
+      <RunningHeader className="panel-header">
+        <Accordion.Toggle as={ToggleButton} variant="link" eventKey={ props.index.toString() }>
+          { props.title }
+        </Accordion.Toggle>
+      </RunningHeader>
+    )
+  }
+
+  return (
+    <Accordion>
+      <AccordionBody>
+        { toggleState() }
+        <Accordion.Collapse eventKey={ props.index.toString() }>
+          <CardBody>{ props.children }</CardBody>
+        </Accordion.Collapse>
+      </AccordionBody>
+    </Accordion>
+  );
+}
+
+export default ControlLoopItem;
