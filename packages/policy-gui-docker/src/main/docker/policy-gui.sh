5f94cc885d6d93e56036243aca6359860d691d93
danielhanrahan <daniel.hanrahan@est.tech>
2021-09-17 18:58:20 +0100

    Disable apex-editor and pdp-monitoring
    Have only nginx process (clamp) running in policy-gui
    
    Issue-ID: POLICY-3650
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: If15d54f83444b845dc23b090cf966afaf1664490

--- packages/policy-gui-docker/src/main/docker/policy-gui.sh
+++ packages/policy-gui-docker/src/main/docker/policy-gui.sh
@@ -19,17 +19,17 @@
 # ============LICENSE_END=========================================================
 #
 
-trap 'exit 0' SIGTERM
+#trap 'exit 0' SIGTERM
 
-JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
+#JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
 
-echo "Starting gui-editor-apex"
-$JAVA_HOME/bin/java -jar "$POLICY_HOME/lib/gui-editor-apex-uber.jar" -p 18989 &
+#echo "Starting gui-editor-apex"
+#$JAVA_HOME/bin/java -jar "$POLICY_HOME/lib/gui-editor-apex-uber.jar" -p 18989 &
 
-echo "Starting gui-pdp-monitoring"
-$JAVA_HOME/bin/java -jar "$POLICY_HOME/lib/gui-pdp-monitoring-uber.jar" -p 17999 &
+#echo "Starting gui-pdp-monitoring"
+#$JAVA_HOME/bin/java -jar "$POLICY_HOME/lib/gui-pdp-monitoring-uber.jar" -p 17999 &
 
 echo "Starting nginx"
-nginx -g "daemon on;"
+nginx -g "daemon off;"
 
-wait
+#wait
