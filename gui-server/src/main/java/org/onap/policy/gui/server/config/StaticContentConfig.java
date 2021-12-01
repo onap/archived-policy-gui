afa10341a120f1d0ee12119645b52c3dc7d7b71f
danielhanrahan <daniel.hanrahan@est.tech>
2021-12-01 16:45:08 +0000

    Use Spring Boot for policy-gui
    Remove nginx from docker image
    Create gui-server module serving clamp gui using Spring
    Add Spring Gateway filter to pass client SSL cert to clamp backend
    Write tests for Spring Gateway client SSL filter class
    
    Issue-ID: POLICY-3638
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: Ie857f5755015c522b8cf781de255f7a1d567e263

---
+++ gui-server/src/main/java/org/onap/policy/gui/server/config/StaticContentConfig.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.gui.server.config;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class StaticContentConfig {
+
+    /**
+     * Adds a route for /clamp/.
+     * @param html Location of static resource (index.html) for clamp
+     * @return route
+     */
+    @Bean
+    public RouterFunction<ServerResponse> clampHtmlRouter(@Value("classpath:/static/clamp/index.html") Resource html) {
+        return route(GET("/clamp/"), request -> ok().contentType(MediaType.TEXT_HTML).bodyValue(html));
+    }
+
+}
