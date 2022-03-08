da7fa9387551b80196de5df26f02aee6a0f06cc3
lapentafd <francesco.lapenta@est.tech>
2022-03-08 09:25:43 +0000

    Code coverage policy gui react
    Issue-ID: POLICY-3351
    Signed-off-by: lapentafd <francesco.lapenta@est.tech>
    Change-Id: I5329a7437be4702361940ef9e22f51413ed892d0

---
+++ gui-clamp/ui-react/src/index.test.js
@@ -0,0 +1,45 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2022 Nordix Foundation. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, MemoryRouter } from 'react-router-dom'
+import OnapClamp from './OnapClamp';
+
+
+
+const routing = (
+  <MemoryRouter forceRefresh={ false }>
+    <Route path="/" component={ OnapClamp }/>
+  </MemoryRouter>
+);
+jest.mock("react-dom", () => ({ render: jest.fn() }));
+
+describe("Application root", () => {
+  it("should render without crashing", () => {
+    const div = document.createElement("div");
+    div.id = "root";
+    document.body.appendChild(div);
+    require("./index.js");
+    expect(ReactDOM.render).toHaveBeenCalledWith(routing, div);
+  });
+});
