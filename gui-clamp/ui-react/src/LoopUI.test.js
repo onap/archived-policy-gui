09333b66b5bb72cc73986346e55e113f2725e070
lapentafd <francesco.lapenta@est.tech>
2022-03-08 09:25:43 +0000

    Code coverage policy gui react
    Issue-ID: POLICY-3351
    Signed-off-by: lapentafd <francesco.lapenta@est.tech>
    Change-Id: I5329a7437be4702361940ef9e22f51413ed892d0

--- gui-clamp/ui-react/src/LoopUI.test.js
+++ gui-clamp/ui-react/src/LoopUI.test.js
@@ -4,6 +4,7 @@
  * ================================================================================
  * Copyright (C) 2019 AT&T Intellectual Property. All rights
  *                             reserved.
+ * Modifications Copyright (C) 2022 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +30,14 @@ import LoopCache from './api/LoopCache';
 import LoopActionService from './api/LoopActionService';
 import LoopService from './api/LoopService';
 
+import { ThemeConsumer } from 'styled-components'
+import { GlobalClampStyle } from './theme/globalStyle.js';
+import { DefaultClampTheme } from './theme/globalStyle.js';
+
+export const shallowWithTheme = (children, theme = DefaultClampTheme) => {
+  ThemeConsumer._currentValue = theme
+  return shallow(children)
+}
 describe('Verify LoopUI', () => {
   beforeEach(() => {
     fetch.resetMocks();
@@ -169,5 +178,10 @@ describe('Verify LoopUI', () => {
     expect(component.state('showSucAlert')).toEqual(false);
     expect(component.state('showFailAlert')).toEqual(true);
     expect(component.state('showMessage')).toEqual("testAlert2");
+  })
+
+  test('Test renders correctly Clamp Style', () => {
+    let tree = shallowWithTheme(<GlobalClampStyle />);
+    expect(tree).toMatchSnapshot();
   })
 });
